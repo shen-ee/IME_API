b7d11d9f5cc6099cf875c73488f4d1d9bc77a83c
Shen Yi <jerry31@qq.com>
2018-04-22 17:04:44 +0800

    change nothing

--- IME_API.py
+++ IME_API.py
@@ -2,10 +2,6 @@
 import requests
 import json
 
-
-pure_char=" "
-
-
 def is_alphabet(uchar):
     """判断一个unicode是否是英文字母"""
     if (uchar >= u'\u0041' and uchar <= u'\u005a') or (uchar >= u'\u0061' and uchar <= u'\u007a'):
